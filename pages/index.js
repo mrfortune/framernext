98faee6e56c34bbb3eb3b084c50bd228ee3fae82
Robert Harris <rharris@worldshaker.com>
2022-10-06 02:57:16 -0400

    commit

--- pages/index.js
+++ pages/index.js
@@ -1,152 +1,244 @@
-import { Button, Box, CardActionArea, CardActions, Stack, Card, Item, Container} from '@mui/material';
-import CardContent from '@mui/material/CardContent';
-import CardMedia from '@mui/material/CardMedia';
+import {
+  Button,
+  Box,
+  CardActionArea,
+  CardActions,
+  Stack,
+  Card,
+  Item,
+  Container,
+} from "@mui/material";
+import CardContent from "@mui/material/CardContent";
+import CardMedia from "@mui/material/CardMedia";
 import { Typography } from "@mui/material";
-import Grid from '@mui/material/Grid';
-import { Paper } from '@mui/material';
-import Image from 'next/image';
-import Link from 'next/link';
-import { motion } from "framer-motion"
-import { NextUIProvider } from '@nextui-org/react';
-import Meta from '../components/Meta';
+import Grid from "@mui/material/Grid";
+import { Paper } from "@mui/material";
+import Image from "next/image";
+import Link from "next/link";
+import { motion } from "framer-motion";
+import { NextUIProvider } from "@nextui-org/react";
+import Meta from "../components/Meta";
 
 export default function Home() {
   return (
     <div>
-<Meta title='WorldShaker Interactive:Home'/>
-<Box component="div" className="homeHero" padding={0}  mb={10}  direction="row" display='flex' alignItems='center' justifyContent='center'> 
-<Box component="div" p={2}  sx={{maxWidth:'md'}}>
-
-<Box component="div" sx={{display:'block', width:'60%', height:'30vh'}}>
-<Typography  variant="h1"  mb={2} sx={{color:'#fafafa',}}>
-Data-Driven Design for Humans.
-</Typography>
-<Typography variant="body1" mb={2} sx={{color:'#fafafa', fontWeight:'400', textTransform:'none'}}>
-UX/UI design and engineering practioners who focus on identifying user pain-points and problems and designing solutions that work.
-</Typography>
-<Button color="primary" variant="contained" size="large" sx={{width:'160px', }}>
-<Link href="/story">Learn More...</Link>
-</Button>
-</Box>
-</Box>
-</Box>
-    
-          
-    <Box direction="row" mb={8} display='flex' ml={2} justifyContent='center' component="div">
-     <Grid container spacing={2}  sx={{maxWidth:'md'}}>
-       <Grid item xs={12} sm={12} md={12}  lg={12}>
-         <Typography variant="h2"  sx={{color:'#333', textAlign:'left'}}>
-  What We Do
-</Typography>
-       </Grid>
-       <Grid item xs={12} md={12} sm={12} lg={12}>
-      <Box component="div" ml={0} className="whatWeDo" height='100%' display='flex'>
-
+      <Meta title="WorldShaker Interactive:Home" />
+      <Box
+        component="div"
+        className="homeHero"
+        padding={0}
+        mb={10}
+        direction="row"
+        display="flex"
+        alignItems="center"
+        justifyContent="center"
+      >
+        <Box component="div" p={2} sx={{ maxWidth: "md" }}>
+          <Box
+            component="div"
+            sx={{ display: "block", width: "60%", height: "30vh" }}
+          >
+            <Typography variant="h1" mb={2} sx={{ color: "#fafafa" }}>
+              Data-Driven Design for Humans.
+            </Typography>
+            <Typography
+              variant="body1"
+              mb={2}
+              sx={{
+                color: "#fafafa",
+                fontWeight: "400",
+                textTransform: "none",
+              }}
+            >
+              UX/UI design and engineering practioners who focus on identifying
+              user pain-points and problems and designing solutions that work.
+            </Typography>
+            <Button
+              color="primary"
+              variant="contained"
+              size="large"
+              sx={{ width: "160px" }}
+            >
+              <Link href="/story">Learn More...</Link>
+            </Button>
+          </Box>
+        </Box>
       </Box>
-   
-         </Grid> 
-</Grid>
 
-    </Box>
-
-    
-    <Box component='div' direction="row" mb={10} display='flex' ml={2} justifyContent='center'>
-      <Grid container spacing={2} sx={{maxWidth:'md'}}>
-        <Grid item xs={12} sm={12} md={12} lg={12}>
-      <Typography variant="h2" mb={4} sx={{color:'#333', textAlign:'left'}}>
-Projects
-
-  </Typography>
-  <Typography variant="body1">
-    Worked as a UX Engineer, Project Manager, Web Producer &amp; Designer, and Design Technologist for these brands.
-  </Typography>  
-  </Grid>
-  <Grid item xs={12} md={12} lg={12}>
-    <Box component="div" ml={0} className="ourProjects" height='100%' display='flex'>
-  
-  </Box>
-  </Grid>
-  
-  </Grid>
-    </Box>
- 
-<Box component="div" direction="row" ml={2} mr={2} display='flex' justifyContent='center'>
-<Grid container spacing={2} sx={{maxWidth:'md'}}>
-  <Grid item xs={12} sm={12} md={12} lg={12}>
-<Typography variant="h2" sx={{color:'#333'}}>
-Case Studies
-</Typography>
-  </Grid>
-<Grid item xs={12} md={12} lg={12} mb={8}>
-  <Card square ={false} className="MuiPaper-rounded" sx={{ display: 'flex',}}>
-      <Box p={1} sx={{ display: 'flex', flexDirection: 'column', width:'66.66%' }}>
-        <CardContent sx={{ flex: '1 0 auto',  }}>
-          <Typography component="div" variant="h3" className="cardHeader">
-            Fade Technology Solutions
-          </Typography>
-          <Typography variant="subtitle1" color="text.secondary" mb={3} component="div" >
-            Explore the process of designing a B2B dashboard with data-visualization and ad campaign management tools.
-          </Typography>
-          
-        <Button size="small" variant="text" color="primary">
-          <Link href="/fade-solutions"> Read More...</Link>
-        </Button>
-     
-        </CardContent>
-       
+      <Box
+        direction="row"
+        mb={8}
+        display="flex"
+        ml={2}
+        justifyContent="center"
+        component="div"
+      >
+        <Grid container spacing={2} sx={{ maxWidth: "md" }}>
+          <Grid item xs={12} sm={12} md={12} lg={12}>
+            <Typography variant="h2" sx={{ color: "#333", textAlign: "left" }}>
+              What We Do
+            </Typography>
+          </Grid>
+          <Grid item xs={12} md={12} sm={12} lg={12}>
+            <Box
+              component="div"
+              ml={0}
+              className="whatWeDo"
+              height="100%"
+              display="flex"
+            ></Box>
+          </Grid>
+        </Grid>
       </Box>
-      <CardMedia
-        component="img"
-        height="225"
-        width="300"
-        sx={{ width:300 }}
-        background-size = 'cover'
-        image="/assets/fadecardImage.png"
-        alt="Fade Technology Solutions"
-      />
-    </Card>
 
-</Grid>
-
-<Grid item xs={12} md={12} lg={12} mb={8}>
-<Card square={false} className="MuiPaper-rounded" sx={{ display: 'flex',}}>
-<Box p={1} sx={{ display: 'flex', flexDirection: 'column', width:'66.66%' }}>
-        <CardContent sx={{ flex: '1 0 auto',  }}>
-          <Typography component="div" variant="h3" className="cardHeader">
-           West Side German Shepherd Rescue
-          </Typography>
-          <Typography variant="subtitle1"  color="text.secondary" component="div" mb={3} >
-            Let's walk through the dog adoption process that we explored and the assumptions, insights, and final design of sheprescue.org.
-          </Typography>
-          
-          <Button size="small" variant="text" color="primary">
-          <Link href="/west-side-german-shepherd"> Read More...</Link>
-        </Button>
-       
-        </CardContent>
-    
+      <Box
+        component="div"
+        direction="row"
+        mb={10}
+        display="flex"
+        ml={2}
+        justifyContent="center"
+      >
+        <Grid container spacing={2} sx={{ maxWidth: "md" }}>
+          <Grid item xs={12} sm={12} md={12} lg={12}>
+            <Typography
+              variant="h2"
+              mb={4}
+              sx={{ color: "#333", textAlign: "left" }}
+            >
+              Brands Worked For
+            </Typography>
+            <Typography variant="body1">
+              Worked as a UX Engineer, Project Manager, Web Producer &amp;
+              Designer, and Design Technologist for these brands.
+            </Typography>
+          </Grid>
+          <Grid item xs={12} md={12} lg={12}>
+            <Box
+              component="div"
+              ml={0}
+              className="ourProjects"
+              height="100%"
+              display="flex"
+            ></Box>
+          </Grid>
+        </Grid>
       </Box>
-     
-      <CardMedia
-      component="img"
-      width="300"
-      height="225"
-      sx={{ width:300 }}
-        image="/assets/wgsrcardimage.png"
-        alt="WGSR"
-      />
-      
-    </Card>
 
-</Grid>
+      <Box
+        component="div"
+        direction="row"
+        ml={2}
+        mr={2}
+        display="flex"
+        justifyContent="center"
+      >
+        <Grid container spacing={2} sx={{ maxWidth: "md" }}>
+          <Grid item xs={12} sm={12} md={12} lg={12}>
+            <Typography variant="h2" sx={{ color: "#333" }}>
+              Case Studies
+            </Typography>
+          </Grid>
+          <Grid item xs={12} md={12} lg={12} mb={8}>
+            <Card
+              square={false}
+              className="MuiPaper-rounded"
+              sx={{ display: "flex" }}
+            >
+              <Box
+                p={1}
+                sx={{
+                  display: "flex",
+                  flexDirection: "column",
+                  width: "66.66%",
+                }}
+              >
+                <CardContent sx={{ flex: "1 0 auto" }}>
+                  <Typography
+                    component="div"
+                    variant="h3"
+                    className="cardHeader"
+                  >
+                    Fade Technology Solutions
+                  </Typography>
+                  <Typography
+                    variant="subtitle1"
+                    color="text.secondary"
+                    mb={3}
+                    component="div"
+                  >
+                    Explore the process of designing a B2B dashboard with
+                    data-visualization and ad campaign management tools.
+                  </Typography>
 
-</Grid>
-    </Box>
+                  <Button size="small" variant="text" color="primary">
+                    <Link href="/fade-solutions"> Read More...</Link>
+                  </Button>
+                </CardContent>
+              </Box>
+              <CardMedia
+                component="img"
+                height="225"
+                width="300"
+                sx={{ width: 300 }}
+                background-size="cover"
+                image="/assets/fadecardImage.png"
+                alt="Fade Technology Solutions"
+              />
+            </Card>
+          </Grid>
 
+          <Grid item xs={12} md={12} lg={12} mb={8}>
+            <Card
+              square={false}
+              className="MuiPaper-rounded"
+              sx={{ display: "flex" }}
+            >
+              <Box
+                p={1}
+                sx={{
+                  display: "flex",
+                  flexDirection: "column",
+                  width: "66.66%",
+                }}
+              >
+                <CardContent sx={{ flex: "1 0 auto" }}>
+                  <Typography
+                    component="div"
+                    variant="h3"
+                    className="cardHeader"
+                  >
+                    West Side German Shepherd Rescue
+                  </Typography>
+                  <Typography
+                    variant="subtitle1"
+                    color="text.secondary"
+                    component="div"
+                    mb={3}
+                  >
+                    Let's walk through the dog adoption process that we explored
+                    and the assumptions, insights, and final design of
+                    sheprescue.org.
+                  </Typography>
 
+                  <Button size="small" variant="text" color="primary">
+                    <Link href="/west-side-german-shepherd"> Read More...</Link>
+                  </Button>
+                </CardContent>
+              </Box>
 
- 
- </div>
-
-  )
-};
+              <CardMedia
+                component="img"
+                width="300"
+                height="225"
+                sx={{ width: 300 }}
+                image="/assets/wgsrcardimage.png"
+                alt="WGSR"
+              />
+            </Card>
+          </Grid>
+        </Grid>
+      </Box>
+    </div>
+  );
+}
--- pages/story.js
+++ pages/story.js
@@ -1,74 +1,119 @@
 import * as React from "react";
-import { styled } from '@mui/material/styles';
-import Box from '@mui/material/Box';
-import Container from '@mui/material/Container';
-import Paper from '@mui/material/Paper';
-import Grid from '@mui/material/Grid';
-import Typography from '@mui/material/Typography';
-import DownloadIcon from '@mui/icons-material/Download';
-import LinkedInIcon from '@mui/icons-material/LinkedIn';
-import Link from 'next/link';
-import Meta from '../components/Meta';
-
+import { styled } from "@mui/material/styles";
+import Box from "@mui/material/Box";
+import Container from "@mui/material/Container";
+import Paper from "@mui/material/Paper";
+import Grid from "@mui/material/Grid";
+import Typography from "@mui/material/Typography";
+import DownloadIcon from "@mui/icons-material/Download";
+import LinkedInIcon from "@mui/icons-material/LinkedIn";
+import Link from "next/link";
+import Meta from "../components/Meta";
 
 const Story = () => {
-	return(
-		<div>
-
-		<Meta title='Story'/>
-		<Box component="div" mt={15} mb={15} direction="row" display='flex' justifyContent='center'>
-	
-			<Grid container pl={1} spacing={2} sx={{paddingLeft:'16px', maxWidth: 'md'}}>
-				<Grid item xs={11} md={8} lg={9}>
-<Typography variant="h1" className="header" mb={6}>Story</Typography>
-			<Typography variant="h2" mb={2}><Box component="span" sx={{fontWeight:'400', }}>Robert S. Harris</Box></Typography>
-			<Typography variant="subtitle1" mb={1} sx={{textTransform:'none', fontWeight:'600',}}>UX Engineer &amp; Designer | Design Thinking Practitioner | Aspiring Product Manager</Typography>
-			<Box component="span"  display='flex' alignItems='center' sx={{
-        '& > :not(style)': {
-          mr: 1,
-        },
-      }}><Link href="RH-UX.pdf" ><a><DownloadIcon sx={{ fontSize: 40 }}/></a></Link><Link href="http://linkedin.com/in/robertsharris"><a><LinkedInIcon sx={{ fontSize: 40 }}/></a></Link></Box>
-		  </Grid>
-		  <Grid item mt={2} xs={11} md={8} lg={9}>
-			  <Typography mb={2} variant="body1">
-			  I am a UX engineer, architech, designer and design thinking practitioner.
-		I have worked for large brands successfully identifying user problems and pain-points and
-			  delivering products and solutions for their customers and accomplishing business goals. 
-			  I work in collaboration with stakeholders, and cross-functional teams to deliver and launch solutions. 
-  
-	  </Typography>
-	  <Typography mb={2} variant="body1">
-	  My love for design began as a child where I would sketch for hours, and I developed a love for drawing characters and creating figures with clay. I was always trying to create something new and interesting. That creative interest evolved into music, video and photography, so when the Web became ubiquitous it was obvious how tech, business and creative work would merge into an interactive experience. I did not realize it at the time but that was my first insight into the world of user experience.
-  
-	  </Typography>
-	  <Typography mb={2} variant="body1">
-	  My degree in business trained me to think about marketing, management, statistics, and the challenges involved with succeeding in business. I learned to develop websites, and then I learned online video production, photography and how to translate compositions into code. 
-	  </Typography>
-	  <Typography mb={2} variant="body1">
-	  WorldShaker Interactive was founded to solve a problem that my cousin and I had identified, wrote a complete business plan for and designed a mock-up. Our plan did not get funded but I see it everytime I visit SoundCloud. I'm not saying that they stole our plan, but their business model for artists is basically what we had developed. Glad to see that our assumptions proved to be right and a solution for many independent artists and users to enjoy unsigned artists. 
-  
-	  </Typography>
-	  <Typography mb={2} variant="body1">
-	  I am still designing soltuons to problems and who knows may come up with another idea that can shake the world. But, for now, I am happy to share my skills with companies small, medium and large to help them deliver better user experiences to their customers.	  
-		</Typography>
-		</Grid>
-</Grid>
-<Grid item xs={1} md={4} lg={3}>
-
-</Grid>	
-
-		
-			
-			
-		</Box>
-		</div>
-	)
-}
-export default Story
-
-
-
-	
-
-		
-			
+  return (
+    <div>
+      <Meta title="Story" />
+      <Box
+        component="div"
+        mt={15}
+        mb={15}
+        direction="row"
+        display="flex"
+        justifyContent="center"
+      >
+        <Grid
+          container
+          pl={1}
+          spacing={2}
+          sx={{ paddingLeft: "16px", maxWidth: "md" }}
+        >
+          <Grid item xs={11} md={8} lg={9}>
+            <Typography variant="h1" className="header" mb={6}>
+              Story
+            </Typography>
+            <Typography variant="h2" mb={2}>
+              <Box component="span" sx={{ fontWeight: "400" }}>
+                Robert S. Harris
+              </Box>
+            </Typography>
+            <Typography
+              variant="subtitle1"
+              mb={1}
+              sx={{ textTransform: "none", fontWeight: "600" }}
+            >
+              UX Engineer &amp; Designer | Design Thinking Practitioner |
+              Aspiring Product Manager
+            </Typography>
+            <Box
+              component="span"
+              display="flex"
+              alignItems="center"
+              sx={{
+                "& > :not(style)": {
+                  mr: 1,
+                },
+              }}
+            >
+              <Link href="RH-UX.pdf">
+                <a>
+                  <DownloadIcon sx={{ fontSize: 40 }} />
+                </a>
+              </Link>
+              <Link href="http://linkedin.com/in/robertsharris">
+                <a>
+                  <LinkedInIcon sx={{ fontSize: 40 }} />
+                </a>
+              </Link>
+            </Box>
+          </Grid>
+          <Grid item mt={2} xs={11} md={8} lg={9}>
+            <Typography mb={2} variant="body1">
+              I am a UX engineer, architech, designer and design thinking
+              practitioner. I have worked for large brands successfully
+              identifying user problems and pain-points and delivering products
+              and solutions for their customers and accomplishing business
+              goals. I work in collaboration with stakeholders, and
+              cross-functional teams to deliver and launch solutions.
+            </Typography>
+            <Typography mb={2} variant="body1">
+              My love for design began as a child where I would sketch for
+              hours, and I developed a love for drawing characters and creating
+              figures with clay. I was always trying to create something new and
+              interesting. That creative interest evolved into music, video and
+              photography, so when the Web became ubiquitous it was obvious how
+              tech, business and creative work would merge into an interactive
+              experience. I did not realize it at the time but that was my first
+              insight into the world of user experience.
+            </Typography>
+            <Typography mb={2} variant="body1">
+              My degree in business trained me to think about marketing,
+              management, statistics, and the challenges involved with
+              succeeding in business. I learned to develop websites, and then I
+              learned online video production, photography and how to translate
+              compositions into code.
+            </Typography>
+            <Typography mb={2} variant="body1">
+              WorldShaker Interactive was founded to solve a problem that my
+              cousin and I had identified, wrote a complete business plan for
+              and designed a mock-up. Our plan did not get funded but I see it
+              everytime I visit SoundCloud. I'm not saying that they stole our
+              plan, but their business model for artists is basically what we
+              had developed. Glad to see that our assumptions proved to be right
+              and a solution for many independent artists and users to enjoy
+              unsigned artists.
+            </Typography>
+            <Typography mb={2} variant="body1">
+              I am still designing soltuons to problems and who knows may come
+              up with another idea that can shake the world. But, for now, I am
+              happy to share my skills with companies small, medium and large to
+              help them deliver better user experiences to their customers.
+            </Typography>
+          </Grid>
+        </Grid>
+        <Grid item xs={1} md={4} lg={3}></Grid>
+      </Box>
+    </div>
+  );
+};
+export default Story;
